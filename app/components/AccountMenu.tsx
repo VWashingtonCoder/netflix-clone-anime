00b05082a79cc959859cc9046c240a93320cbe30
V. Andre Washington <kuraiokamiblaze@gmail.com>
2024-06-13 08:57:27 -0600

    Set up and link account menu

---
+++ app/components/AccountMenu.tsx
@@ -0,0 +1,51 @@
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+
+interface AccountMenuProps {
+  visible?: boolean;
+}
+
+export default function AccountMenu({ visible }: AccountMenuProps) {
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div
+      className="
+                bg-black 
+                w-56 
+                absolute 
+                top-14 
+                right-0 
+                py-5 
+                flex-col 
+                border-2 
+                border-gray-800 
+                flex
+            "
+    >
+      <div className="flex flex-col gap-3">
+        <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
+          <Image
+            className="w-8 rounded-md"
+            src="/images/default-blue.png"
+            alt="profile"
+            width={100}
+            height={100}
+          />
+          <p className="text-white text-sm group-hover/item:underline">
+            Username
+          </p>
+        </div>
+        <hr className="bg-gray-600 border-0 h-px my-4" />
+        <div
+          onClick={() => signOut()}
+          className="px-3 text-center text-white text-sm hover:underline"
+        >
+          Sign out of Nekoflix
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import NavbarItem from "./NavabarItem";
 import MobileMenu from "./MobileMenu";
-import { BsChevronDown } from "react-icons/bs";
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
 import { useCallback, useState } from "react";
+import AccountMenu from "./AccountMenu";
 
 export default function Navbar() {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
 
     const toggleMobileMenu = useCallback(() => {
         setShowMobileMenu((current) => !current)
+    }, [])
+
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu((current) => !current)
     }, [])
 
   return (
@@ -60,6 +66,23 @@ export default function Navbar() {
             <p className="text-white text-sm">Browse</p>
             <BsChevronDown className="text-white transition"/>
             <MobileMenu visible={showMobileMenu} />
+        </div>
+
+        <div className="flex flex-row ml-auto gap-7 items-center">
+            <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                <BsSearch />
+            </div>
+            <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                <BsBell />
+            </div>
+
+            <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
+                <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+                    <Image src="/images/default-blue.png" alt="profile" width={100} height={100} />
+                </div>
+                <BsChevronDown className="text-white transition" />
+                <AccountMenu visible={showAccountMenu} />
+            </div>
         </div>
       </div>
     </nav>
