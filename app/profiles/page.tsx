de32da1a7f795552845e617292a179e5494875c2
V. Andre Washington <kuraiokamiblaze@gmail.com>
2024-06-13 06:44:37 -0600

    Build profile page

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,14 +1,56 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
+import Image from "next/image"
 
 const Profiles = () => {
-  const { status } = useSession();
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const user = session?.user
   if (status === "unauthenticated") redirect("/auth");
 
   return (
-    <div>
-      <p className="text-white text-4xl">Profiles</p>
+    <div className="flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">Who is watching?</h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => {router.push("/")}}>
+            
+            <div className="group flex-row w-44 mx-auto">
+              <div
+                className="
+                  w-44
+                  h-44
+                  rounded-md
+                  flex
+                  items-center
+                  justify-center
+                  border-2
+                  border-transparent
+                  group-hover:cursor-pointer
+                  group-hover:border-white
+                  overflow-hidden
+                "
+              >
+                <Image src="/images/default-blue.png" alt="Profile" width={300} height={300} />
+              </div>
+
+              <div
+                className="
+                  mt-4
+                  text-gray-400
+                  text-2xl
+                  text-center
+                  group-hover: text-white
+                "
+              >
+                {user?.name}
+              </div>
+            </div>
+          
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
