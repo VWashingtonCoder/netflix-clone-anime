8ad9cbe8e1bc584eff30a343d2832e98c1b65986
V. Andre Washington <kuraiokamiblaze@gmail.com>
2024-07-11 19:57:38 -0600

    Update seed script video urls and descriptions

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,11 +6,13 @@ async function main() {
   console.log(`Seeding started...🌱`)
   const movie1 = await prisma.movie.upsert({
     where: { id: 1 },
-    update: {},
+    update: {
+      description: "Hiroto Shinohara, a new transfer student to Academy Island, inadvertently challenges Sarasa Saionji, the island's revered Empress, on his first day. He wins a crucial game through a bluff, but must now maintain a facade of being a high-ranking Seven Star student to avoid expulsion. Assisted by the secretive Company led by Shirayuki Himeji, Hiroto navigates challenges from jealous peers while searching for a missing girl from his past, all while hiding his true One Star status."
+    },
     create: {
       title: "Liar Liar",
-      description: `Hiroto Shinohara is a new transfer student to Academy Island, where games determine student rankings. The more games won, the more stars a student has, with the highest ranking being that of a Seven Star. On Hiroto's first day, he accidentally antagonizes Sarasa Saionji, the so-called Empress of Academy Island and granddaughter of the island's head director. Forced into a game fueled by misunderstandings that he somehow ends up winning, he is thrust into a major bluff where he must lie about his ranking or risk expulsion from the school and island. Fortunately, with the help of the mysterious Company headed by Shirayuki Himeji, Hiroto is not alone in his bluff. The Company will do everything in its power to help him cheat his way through the game challenges sent his way by students wanting to beat him. Though he is a One Star student, he must fight as though he is actually a Seven Star, and overcome insurmountable odds in his quest to search for a missing girl from his past.`,
-      videoUrl: "https://www.youtube.com/embed/tW-hx-sQOYw?si=lYfgRCe8_iigHDFe",
+      description: "Hiroto Shinohara, a new transfer student to Academy Island, inadvertently challenges Sarasa Saionji, the island's revered Empress, on his first day. He wins a crucial game through a bluff, but must now maintain a facade of being a high-ranking Seven Star student to avoid expulsion. Assisted by the secretive Company led by Shirayuki Himeji, Hiroto navigates challenges from jealous peers while searching for a missing girl from his past, all while hiding his true One Star status.",
+      videoUrl: "https://www.dropbox.com/scl/fi/xwew9ntgwjmlp3u4rzsvn/liar-liar-season-1-episode-1_HD.mp4?rlkey=3e3xfh0lq2dne9kgr0jm5zq9h&st=lwihsjre&raw=1",
       thumbnailUrl: "https://cdn.myanimelist.net/images/anime/1571/134525.jpg",
       genre: "Suspense",
       duration: "4 hours",
@@ -19,14 +21,14 @@ async function main() {
 
   const movie2 = await prisma.movie.upsert({
     where: { id: 2 },
-    update: {},
+    update: {
+      description: "Al Wayne, striving to excel as a farmer, achieves top-tier cultivation skills and becomes a first-rate cultivator. His newfound prowess surpasses even legendary heroes, prompting the royal family to seek his aid against mounting disasters. Reluctant yet determined to protect his fields, Al accepts the responsibility of defending against these threats."
+    },
     create: {
       title:
         "I've Somehow Gotten Stronger When I Improved My Farm-Related Skills",
-      description: `Al Wayne's greatest goal is to become the best farmer he can be. With utmost effort, he reaches the apex of his farming skills and earns the title of a first-rate cultivator. Due to this achievement, Al gains a substantial upgrade to his stats which now eclipse even the strength of legendary heroes of old.
-      
-      Soon after discovering his potential, the royal family enlists his help against the rising number of disasters that coincidentally began right as his true power was awakened. Although reluctant, Al concedes that in order to protect his precious fields, he must do his best to keep all threats at bay.`,
-      videoUrl: "https://youtu.be/aszQti5MOVg?si=2KcRxzPo4xiNNRPm",
+      description: "Al Wayne, striving to excel as a farmer, achieves top-tier cultivation skills and becomes a first-rate cultivator. His newfound prowess surpasses even legendary heroes, prompting the royal family to seek his aid against mounting disasters. Reluctant yet determined to protect his fields, Al accepts the responsibility of defending against these threats.",
+      videoUrl: "https://www.dropbox.com/scl/fi/kv0nbqiuq8c1kfxvc68p1/I-ve-Somehow-Gotten-Stronger-When-I-Improved-My-Farm-Related-Skills-Episode-_Full-HD.mp4?rlkey=ooydfeapconqa8y0o8rkpv8hs&st=41069124&raw=1",
       thumbnailUrl: "https://cdn.myanimelist.net/images/anime/1384/127328.jpg",
       genre: "Action, Adventure, Fantasy",
       duration: "3 hours 53 minutes",
@@ -35,13 +37,13 @@ async function main() {
 
   const movie3 = await prisma.movie.upsert({
     where: { id: 3 },
-    update: {},
+    update: {
+      description: "Kelvin prepares for reincarnation in another world by sacrificing memories for powerful abilities and an S-class summoner title from goddess Melfina. He chooses Melfina as his companion, banking on his love for her to endure despite losing memories. To summon her physical form, Kelvin must gain mana points through battles and contracts with powerful allies, beginning his adventurous journey guided by Melfina."
+    },
     create: {
       title: "Black Summoner",
-      description: `To prepare for reincarnation in another world, Kelvin gives up select memories from his previous life in exchange for powerful abilities, additional skill points, and an S-class summoner title. As a bonus, the goddess facilitating his rebirth, Melfina, offers him a choice of any companion to give him a head start in his summoner role. Kelvin—who has fallen head over heels for Melfina at first sight—promptly chooses the deity, confident that his passionate feelings for her will resurface even without all of his memories.
-      
-      Kelvin embarks on his exciting new journey with Melfina as his guide. However, in order to summon his beloved goddess' physical form, he needs to acquire significant amounts of mana points—and the best way to accomplish this is to level up by fighting strong enemies and making contracts with even stronger companions.`,
-      videoUrl: "https://youtu.be/pRQ0bGDNBoQ?si=WJlBWlLNttZYCNiH",
+      description: "Kelvin prepares for reincarnation in another world by sacrificing memories for powerful abilities and an S-class summoner title from goddess Melfina. He chooses Melfina as his companion, banking on his love for her to endure despite losing memories. To summon her physical form, Kelvin must gain mana points through battles and contracts with powerful allies, beginning his adventurous journey guided by Melfina.",
+      videoUrl: "https://www.dropbox.com/scl/fi/elzdqfyf42stb3yz50b2t/Black-summoner-episode-1-English-Dubbed_High.mp4?rlkey=j7fvt4jen3tacinwiyic22pq7&st=yh8o5p4y&raw=1",
       thumbnailUrl: "https://cdn.myanimelist.net/images/anime/1517/125496.jpg",
       genre: "Action, Fantasy",
       duration: "3 hours 50 mins",
@@ -50,17 +52,13 @@ async function main() {
 
   const movie4 = await prisma.movie.upsert({
     where: { id: 4 },
-    update: {},
+    update: {
+      description: `"To Your Eternity" follows an immortal being, initially an orb, sent to Earth to observe. It transforms through reflections it captures, starting as a rock and then moss. After taking the form of a dying wolf, it gains consciousness and encounters a lonely boy searching for his tribe in a paradise. Together, they embark on a journey, unsure of what lies ahead.`
+    },
     create: {
       title: "To Your Eternity",
-      description: `An Orb, known only as It, is cast to Earth to be observed from afar. Capable of changing forms from beings whose reflections It captures, It first becomes a rock and then, due to the rising temperature, moss.
-      
-      It does not move until one snowy day, a wolf at death's door barely crosses by. When It takes the animal's form, It attains awareness of its consciousness and starts to wander with an unclear destination in mind. Soon, It comes across the wolf's master—a young boy waiting for his tribe to return from a paradise abundant with fish and fruit in the south. Although the boy is lonely, he still hopes those whom he holds dear in his memories have not forgotten him and that he will reunite with them one day.
-      
-      The boy wants to explore new surroundings and decides to abandon his home with It to find the paradise using the traces his tribe left behind. However, with a heavily injured body and no sight of his elder comrades, what will become of the boy?
-      
-      Fumetsu no Anata e illustrates the story of an immortal being experiencing humanity, meeting all types of people in many places throughout time.`,
-      videoUrl: "https://youtu.be/W12xMjwY4UE?si=LLX9K37h8Y6T746V",
+      description: `"To Your Eternity" follows an immortal being, initially an orb, sent to Earth to observe. It transforms through reflections it captures, starting as a rock and then moss. After taking the form of a dying wolf, it gains consciousness and encounters a lonely boy searching for his tribe in a paradise. Together, they embark on a journey, unsure of what lies ahead.`,
+      videoUrl: "https://www.dropbox.com/scl/fi/5qubfbtzuiil4wnd22zko/To-Your-Eternity-Ep.-1-_-DUB-_-The-Last-One_HD.mp4?rlkey=gb8v9zuyeowt7t1sx41dpw8s1&st=ruo1h9al&raw=1",
       thumbnailUrl: "https://cdn.myanimelist.net/images/anime/1880/118484.jpg",
       genre: "Adventure, Drama, Supernatural",
       duration: "7 hours",
